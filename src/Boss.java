e4357c3a70de5af4daf9dc2af3860443f4a44ac1
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2022-12-17 10:26:21 +0300

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Boss {
+public class Boss extends GameEntity{
+    Weapon weaponType = new Weapon();
+
+    public Boss(int health, int damage) {
+        super(health, damage);
+    }
+
+    public Weapon getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(Weapon weaponType) {
+        this.weaponType = weaponType;
+    }
+
+    public String info () {
+        return getType() + getDamage() + weaponType.getWeaponName() + getWeaponType();
+    }
 }
