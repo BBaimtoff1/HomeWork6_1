ad54e8de7bd698ef7defbadd2bec64112ae5c6eb
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2022-12-17 10:27:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Boss boss = new Boss(500, 50);
+        boss.weaponType.setWeaponType("weapon");
+        boss.weaponType.setWeaponName("Infinity stones");
+        System.out.println(boss.info());
+
+
+        Skeleton skeleton = new Skeleton(100, 50, 20);
+        skeleton.weaponType.setWeaponType("sword");
+        skeleton.weaponType.setWeaponName("katana");
+        System.out.println(skeleton.info());
+
+
+        Skeleton skeleton1 = new Skeleton(150, 70, 40);
+        skeleton1.weaponType.setWeaponType("sword");
+        skeleton1.weaponType.setWeaponName("tecak");
+        System.out.println(skeleton1.info());
     }
 }
