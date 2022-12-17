f53c403aefaec5dbf11eb3e7bfc90bf4077f0fb8
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2022-12-17 10:26:48 +0300

    Update GameEntity.java

--- src/GameEntity.java
+++ src/GameEntity.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class GameEnity {
+class GameEntity {
+    public int getType() {
+        return Health;
+    }
+
+    public void setType(int type) {
+        this.Health = type;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public GameEntity(int type, int damage) {
+        this.Health = type;
+        this.damage = damage;
+    }
+
+    private int Health;
+
+    private int damage;
 }
